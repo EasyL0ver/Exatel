da2f52f6fdd3c01c7d7e084bbf66e8f8f42e775d
Alkapivo <aleksander.nowak.id@gmail.com>
2019-09-15 03:37:02 +0200

    merge

---
+++ run.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+print('First argument:', sys.argv[0]);
+
+execfile('crawl.py');
