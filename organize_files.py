7fa1ce0119066f7c94016774a0c3d0bc9c066be1
Paweł Kudłacik <pakudlacik@gmail.com>
2019-09-15 10:20:03 +0200

    logging added

---
+++ organize_files.py
@@ -0,0 +1,33 @@
+from datamodel import FileInfo
+
+import os
+
+
+def organize_files(db_session, organize_path):
+    all_files = db_session.query(FileInfo).filter(FileInfo.cluster_id != None).order_by(FileInfo.cluster_id).all()
+
+    if not os.path.exists(organize_path):
+        os.mkdir(organize_path)
+
+    for file in all_files:
+        file_path = file.filepath
+        target_path = organize_path + '/' + str(file.cluster_id)
+
+        if not os.path.exists(target_path):
+            os.mkdir(target_path)
+
+        file_target_path = target_path + '/' + file.filename
+
+        for retry in range(100):
+            try:
+                if os.path.exists(file_target_path):
+                    os.remove(file_target_path)
+
+                os.rename(file_path, file_target_path)
+                break
+            except:
+                print
+                "rename failed, retrying..."
+        file.filepath = file_target_path
+    db_session.commit()
+
--- run.py
+++ run.py
@@ -5,17 +5,19 @@ import cluster
 import crawl
 import silhouette
 import view_db
+import organize_files
 from database import DBProvider
 
 
-path = './sample_data'
-crawl_batch_size = 30000
+path = '.\sample_data'
+organize_folder = './../organized'
+crawl_batch_size = 300
 vectorizer = HashingVectorizer(n_features=1000)
 db = DBProvider(clear_database=True)
 n_cluster_range = range(5, 10)
 elbow_set_size = 30
 max_iterations = 100
-cluster_batch_size = 1000
+cluster_batch_size = 300
 print_db = False
 
 
@@ -26,3 +28,5 @@ cluster.run(cluster_batch_size, cluster_means, db.get_session(), n_clusters=n_cl
 
 if print_db:
     view_db.run(db.get_session())
+
+organize_files.organize_files(db.get_session(), organize_folder)
